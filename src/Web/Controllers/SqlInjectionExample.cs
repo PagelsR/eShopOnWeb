0a954122f7fc2a0f130f7977800185d15f532fac
Randy Pagels <rpagels@xpirit.com>
2023-07-24 17:55:03 +0000

    Updated SqlInjectionExample.cs

--- src/Web/Controllers/SqlInjectionExample.cs
+++ src/Web/Controllers/SqlInjectionExample.cs
@@ -1,1 +1,44 @@
-x
+using System;
+using Microsoft.Data.SqlClient;
+
+namespace Microsoft.eShopWeb.Web;
+
+public class SqlInjectionExample
+{
+    private readonly string _connectionString;
+
+    public SqlInjectionExample(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public bool AuthenticateUser(string username, string password)
+    {
+        string query = "SELECT * FROM Users WHERE Username = '" + username + "' AND Password = '" + password + "'";
+
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    reader.Close();
+                    return true;
+                }
+
+                reader.Close();
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return false;
+            }
+        }
+    }
+}
